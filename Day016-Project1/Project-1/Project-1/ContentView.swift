63a41411f8f030969615171f9bd4d24bd2611572
100rabhcsmc <saurabhchavan052@gmail.com>
2022-08-23 21:42:51 +0530

    Project1

---
+++ Day016-Project1/Project-1/Project-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-1
+//
+//  Created by Saurabh Chavan on 23/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
