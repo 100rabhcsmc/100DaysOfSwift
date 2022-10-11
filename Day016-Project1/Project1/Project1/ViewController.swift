966c7490c9c85bbec2e3b5fd9fe7ee3ea69b3b1b
100rabhcsmc <saurabhchavan052@gmail.com>
2022-10-11 22:53:44 +0530

    TableViewController added

--- Day016-Project1/Project1/Project1/ViewController.swift
+++ Day016-Project1/Project1/Project1/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var pictures = [String]()
 
     override func viewDidLoad() {
@@ -24,7 +24,16 @@ class ViewController: UIViewController {
         print(pictures)
         // Do any additional setup after loading the view.
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 
 }
 
